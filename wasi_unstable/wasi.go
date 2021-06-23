367fb687068ee4284c6450505c72a835a5d83192
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2021-06-23 14:18:22 +0200

    wasi write file

--- wasi_unstable/wasi.go
+++ wasi_unstable/wasi.go
@@ -42,56 +42,89 @@ func Fd_read(fd, p, niovec, written int32) int32 {
 		}
 		return 1
 	}
-	b, o := files[fd]
+	f, o := files[fd]
 	if o == false {
 		return 1
 	}
-	copy(module.Bytes[addr:], b[:n])
-	files[fd] = b[n:]
+	copy(module.Bytes[addr:], f.b[:n])
+	f.b = f.b[n:]
+	files[fd] = f
 	module.SetI32(written, n)
 	return 0
 }
 func Fd_write(fd, p, niovec, written int32) int32 {
+	addr := module.I32(p)
+	n := module.I32(4 + p)
+	b := module.Bytes[addr : addr+n]
 	if fd == 1 {
-		addr := module.I32(p)
-		n := module.I32(4 + p)
-		nw, err := os.Stdout.Write(module.Bytes[addr : addr+n])
+		nw, err := os.Stdout.Write(b)
 		module.SetI32(written, int32(nw))
 		if err == nil {
 			return 0
 		}
+	} else {
+		f, o := files[fd]
+		if o == false {
+			return 1
+		}
+		f.b = append(f.b, b...)
+		files[fd] = f
+		module.SetI32(written, int32(len(b)))
+		return 0
 	}
 	return 1
 }
 func Fd_seek(fp int32, offset int64, whence int32, rp int32) int32 {
-	b, o := files[fp]
+	f, o := files[fp]
 	if o == false {
 		return 1
 	}
 	if whence == 2 {
-		module.SetI32(rp, int32(len(b)))
+		module.SetI32(rp, int32(len(f.b)))
 	} else {
 		module.SetI32(rp, 0)
 	}
 	return 0
 }
 
-var files map[int32][]byte
+type openfile struct {
+	name string
+	b    []byte
+}
+
+var files map[int32]openfile
 
 func Path_open(fd, dirflags, path, pathlen, oflags int32, baserights, inheritrights int64, fdflags, newfp int32) int32 {
-	b, e := ioutil.ReadFile(string(module.Bytes[path : path+pathlen]))
-	if e != nil {
-		fmt.Println(e)
-		return 1
-	}
 	fp := int32(2 + len(files))
-	files[fp] = b
+	name := string(module.Bytes[path : path+pathlen])
+	if oflags == 0 { // assume read
+		b, e := ioutil.ReadFile(name)
+		if e != nil {
+			fmt.Println(e)
+			return 1
+		}
+		files[fp] = openfile{b: b}
+	} else { // assume write file
+		files[fp] = openfile{name: name, b: make([]byte, 0)}
+	}
 	module.SetI32(newfp, fp)
 	return 0
 }
 
-func Fd_close(x int32) int32 { delete(files, x); return 0 }
-func Proc_exit(x int32)      { os.Exit(int(x)) }
+func Fd_close(x int32) int32 {
+	f, o := files[x]
+	if o == false {
+		return 1
+	}
+	if f.name != "" {
+		if os.WriteFile(f.name, f.b, 0644) != nil {
+			return 1
+		}
+	}
+	delete(files, x)
+	return 0
+}
+func Proc_exit(x int32) { os.Exit(int(x)) }
 
 func Args_sizes_get(np, sp int32) int32 {
 	s, n := getargs()
@@ -115,5 +148,5 @@ func getargs() (r []byte, n int32) {
 }
 
 func init() {
-	files = make(map[int32][]byte)
+	files = make(map[int32]openfile)
 }
